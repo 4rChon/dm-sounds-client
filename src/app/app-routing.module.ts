382ba53596a011ae962ad9133b57ed18d084ad31
4rChon <bendbug@gmail.com>
2021-02-12 05:03:30 +0100

    Add route for playlist list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TempComponent } from './temp/temp.component';
+import { SelectablePlaylistContainerComponent } from './playlist/selectable-playlist-container/selectable-playlist-container.component';
 
-const routes: Routes = [{ path: 'temp', component: TempComponent }];
+const routes: Routes = [{ path: 'playlists', component: SelectablePlaylistContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
